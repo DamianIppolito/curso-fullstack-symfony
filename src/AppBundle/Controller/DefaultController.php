90dedce353ca36ff9160c763406c77fb2116c99d
Damian Ippolito <dippolito@dimaticasoftware.com>
2017-11-21 09:50:25 +0100

    Seccion 7 - 25 - Ruta y acción para el Login y validar un email

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\Email;
 
 class DefaultController extends Controller
 {
@@ -14,6 +15,29 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
+    }
+
+    public function loginAction(Request $request){
+	    $helpers = $this->get("app.helpers");
+	    $json = $request->get("json", null);
+		if(!is_null($json)){
+			$params = json_decode($json);
+			$email = (isset($params->email)) ? $params->email : null;
+			$password = (isset($params->password)) ? $params->password : null;
+
+			$emailConstraint = new Email();
+			$emailConstraint->message = "Email format not valid!!";
+			$validate_email = $this->get('validator')->validate($email, $emailConstraint);
+
+			if (count($validate_email) == 0 && !is_null($password)){
+				echo "Data Success";
+			}else{
+				echo "Data incorret";
+			}
+		}else{
+			echo "Send JSON with POST";
+			die();
+		}
     }
 
 	public function pruebasAction(Request $request)
