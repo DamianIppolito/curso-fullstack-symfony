feac71b63bff55c3f7287a0a00f55e0af79341aa
Damian Ippolito <dippolito@dimaticasoftware.com>
2017-11-21 09:32:17 +0100

    Seccion 6 - 23 - Crear un servicio en Symfony para los Helpers

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,10 +5,6 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
-use Symfony\Component\Serializer\Serializer;
 
 class DefaultController extends Controller
 {
@@ -22,23 +18,9 @@ class DefaultController extends Controller
 
 	public function pruebasAction(Request $request)
 	{
+		$helpers = $this->get("app.helpers");
 		$em = $this->getDoctrine()->getManager();
 		$users = $em->getRepository('BackendBundle:User')->findAll();
-		return $this->json($users);
-	}
-
-	public function json($data){
-    	$normalizers = array(new GetSetMethodNormalizer());
-    	$encoders = array("json" => new JsonEncoder());
-
-    	$serializer = new Serializer($normalizers,  $encoders);
-    	$json = $serializer->serialize($data, 'json');
-
-    	$response = new Response();
-    	$response->setContent($json);
-		$response->headers->set('Content-Type', 'application/json');
-
-		return $response;
-
+		return $helpers->json($users);
 	}
 }
