550dbe1629c8a7a17048bd1e3c6db47523f19d23
Damian Ippolito <dippolito@dimaticasoftware.com>
2017-12-04 18:03:50 +0100

    Seccion 10 - 45 - Crear el controlador de comentarios

---
+++ src/AppBundle/Controller/CommentController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class CommentController extends Controller {
+	public function newAction() {
+		echo "COMENTARIOS NEW";die();
+	}
+}
