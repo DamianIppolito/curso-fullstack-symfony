d96a51f9cdf4da3ae55965c63f6557b0a59a4d8b
Damian Ippolito <dippolito@dimaticasoftware.com>
2017-12-07 13:04:20 +0100

    Seccion 10 - 46 - Poder añadir comentarios a un vídeo

--- src/AppBundle/Controller/CommentController.php
+++ src/AppBundle/Controller/CommentController.php
@@ -2,10 +2,45 @@
 
 namespace AppBundle\Controller;
 
+use BackendBundle\Entity\Comment;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class CommentController extends Controller {
-	public function newAction() {
-		echo "COMENTARIOS NEW";die();
+	public function newAction(Request $request){
+		$helpers = $this->get('app.helpers');
+		$hash = $request->get('authorization',null);
+		$authCheck = $helpers->authCheck($hash);
+		if($authCheck){
+			$identity = $helpers->authCheck($hash,true);
+			$json = $request->get('json',null);
+			if(!is_null($json)){
+				$params = json_decode($json);
+				$createdAt = new \DateTime('now');
+				$user_id = (isset($identity->sub)) ? $identity->sub : null;
+				$video_id = (isset($params->video_id)) ? $params->video_id : null;
+				$body = (isset($params->body)) ? $params->body : null;
+				if(!is_null($user_id) && !is_null($video_id)){
+					$em = $this->getDoctrine()->getManager();
+					$user = $em->getRepository('BackendBundle:User')->findOneBy(array("id"=>$user_id));
+					$video = $em->getRepository('BackendBundle:Video')->findOneBy(array("id"=>$video_id));
+					$comment = new Comment();
+					$comment->setUser($user);
+					$comment->setVideo($video);
+					$comment->setBody($body);
+					$comment->setCreatedAt($createdAt);
+					$em->persist($comment);
+					$em->flush();
+					$data  = array( "status" => "success", "code" => 200, "msg" => "Comment created!!!" );
+				}else{
+					$data = array("status" => "error", "code" => 400, "msg" => "Comment not created!!!");
+				}
+			}else{
+				$data = array("status" => "error", "code" => 400, "msg" => "Params not valid!!!");
+			}
+		}else{
+			$data = array("status" => "error", "code" => 400, "msg" => "Authorization not valid!!!");
+		}
+		return $helpers->json($data);
 	}
 }
